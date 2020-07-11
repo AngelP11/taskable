2a286f85a9225f93af2d94ea2f0ed5a25626a141
Angel Ponce <angelgabrielponce@gmail.com>
2020-07-11 19:11:58 -0400

    Component kanban creado

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,53 @@
 import React from 'react'
 import { Button } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
+import uuid from 'uuid/v4'
 
-const useStyles = makeStyles({
-  button: {
-    height: 36,
-    padding: '8px 16px 8px 16px',
-    boxShadow: 'none',
-    borderRadius: 4,
-    backgroundColor: '#1a73e8',
-    color: 'white',
-    fontFamily: 'Manrope',
-    textTransform: 'capitalize',
-    fontSize: 14,
-    '&:hover': {
-      cursor: 'pointer',
-      backgroundColor: '#1a73e8',
-      boxShadow: '0 1px 3px 2px rgba(22, 115, 232, .2)'
-    }
-  }
-})
+/* Assets */
+import './styles/Home.css'
+import buttons from './styles/styles'
 
-function Home () {
-    const classes = useStyles()
+/* Components */
+import Kanban from '../components/Kanban'
 
-    return (
-        <Button className={classes.button}>Hablemos</Button>
-    )
+const useStyle = makeStyles(buttons)
+/* <Box border={1} width={300} p={1} borderRadius="borderRadius" mr={2} key={column.id}></Box> */
+
+const items = [
+  { id: uuid(), content: "First task" },
+  { id: uuid(), content: "Second task" },
+  { id: uuid(), content: "Third task" },
+  { id: uuid(), content: "Fourth task" },
+  { id: uuid(), content: "Fifth task" }
+];
+
+const columns = [
+  {
+    id: uuid(), 
+    name: "Requested",
+    items: items
+  },
+  {
+    id: uuid(), 
+    name: "To do",
+    items: []
+  },
+  {
+    id: uuid(), 
+    name: "Doing",
+    items: []
+  },
+]
+
+function Home() {
+  const classes = useStyle();
+      
+  return (
+    <React.Fragment>
+      <Button className={classes.button}>Hablemos</Button>
+      <Kanban columns={columns} />
+    </React.Fragment>
+  )
 }
 
 export default Home
---
+++ src/pages/styles/styles.js
@@ -0,0 +1,20 @@
+const buttons = {
+  button: {
+    height: 36,
+    padding: '8px 16px 8px 16px',
+    boxShadow: 'none',
+    borderRadius: 4,
+    backgroundColor: '#1a73e8',
+    color: 'white',
+    fontFamily: 'Manrope',
+    textTransform: 'capitalize',
+    fontSize: 14,
+    '&:hover': {
+      cursor: 'pointer',
+      backgroundColor: '#1a73e8',
+      boxShadow: '0 1px 3px 2px rgba(22, 115, 232, .2)'
+    }
+  },
+}
+
+export default buttons
