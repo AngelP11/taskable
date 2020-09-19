44fa8746999f3b4d13d248b8618ce10e01cfef75
Angel Ponce <angelgabrielponce@gmail.com>
2020-09-19 14:18:09 -0400

    Updates

---
+++ precache-manifest.19167a7f8e0e5491e99abd40ef0897b2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12a46951d8d4e3039bdba482beb6d21e",
+    "url": "/taskable/index.html"
+  },
+  {
+    "revision": "ab24659dcf0b2dc1eff6",
+    "url": "/taskable/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "d80bc124bf74365f287c",
+    "url": "/taskable/static/css/main.ec512dbc.chunk.css"
+  },
+  {
+    "revision": "ab24659dcf0b2dc1eff6",
+    "url": "/taskable/static/js/2.a51e5e87.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/taskable/static/js/2.a51e5e87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d80bc124bf74365f287c",
+    "url": "/taskable/static/js/main.da5def2b.chunk.js"
+  },
+  {
+    "revision": "e376380cd39f82fb899c",
+    "url": "/taskable/static/js/runtime-main.a4e26c46.js"
+  },
+  {
+    "revision": "6ffbefc66468b90d7af1cbe1e9f13430",
+    "url": "/taskable/static/media/Inter-Light-slnt=0.6ffbefc6.ttf"
+  },
+  {
+    "revision": "515cae74eee4925d56e6ac70c25fc0f6",
+    "url": "/taskable/static/media/Inter-Regular-slnt=0.515cae74.ttf"
+  },
+  {
+    "revision": "6196e0dab83345b15290ee22620358c1",
+    "url": "/taskable/static/media/Manrope-Medium.6196e0da.ttf"
+  }
+]);
