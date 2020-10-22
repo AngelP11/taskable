36e290c1fef75d5eeb4f9d0e9e80ba8846a8c20d
José López <jflo1937@gmail.com>
2020-10-22 13:43:24 -0400

    espacio de 3 en las columnas de register

---
+++ src/pages/HomeOld.js
@@ -0,0 +1,21 @@
+import React from 'react'
+// import { Button, Box, Slider, Typography, Switch } from '@material-ui/core'
+// import { makeStyles, withStyles } from '@material-ui/core/styles'
+
+/* Assets */
+import './styles/Home.css'
+
+import Layout from '../components/Layout'
+
+
+function HomeOld() { 
+  return (
+    <React.Fragment>
+      <Layout>
+        <h4>En construcción</h4>
+      </Layout>
+    </React.Fragment>
+  )
+}
+
+export default HomeOld
